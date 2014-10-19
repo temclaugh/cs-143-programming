db558ba5056a47c3dbff4ad808641cb23b81f894
Luciano Arang <lucianoarango@college.harvard.edu>
2014-10-19 16:28:03 -0700

    firewall

--- firewall.py
+++ firewall.py
@@ -13,6 +13,19 @@ log = core.getLogger()
 policyFile = "%s/pox/pox/misc/firewall-policies.csv" % os.environ[ 'HOME' ]  
 
 ''' Add your global variables here ... '''
+rules = []
+with open(policyFile) as f:
+	content = f.readlines()
+	for idx, line in enumerate(content):
+		if idx == 0:
+			properties = line.split(",")
+			properties[-1] = properties[-1].strip()
+		else:
+			rules.append({})
+			linearray = line.split(",")
+			linearray[-1] = linearray[-1].strip()
+			for prop in properties:
+				rules[idx-1][prop] = linearray[properties.index(prop)]
 
 
 
@@ -25,7 +38,17 @@ class Firewall (EventMixin):
     def _handle_ConnectionUp (self, event):    
         ''' Add your logic here ... '''
         
-
+	
+	for rule in rules:
+		log.debug("Installing rule from %s to %s", rule['mac_0'], rule['mac_1'] )
+	
+		msg = of.ofp_flow_mod()
+		msg.priority = 100
+		msg.match.dl_src = rule['mac_0']
+		msg.match.dl_dst =  rule['mac_1']
+		msg.actions.append(of.ofp_action_output(port = of.OFPP_NONE))	
+		#Send message to switch
+		event.connection.send(msg)
     
         log.debug("Firewall rules installed on %s", dpidToStr(event.dpid))
 
